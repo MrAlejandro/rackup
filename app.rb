557cd2d8cfa4518792288b0cad6a0db8a6231872
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2019-03-30 09:43:19 +0400

    Rack compatible application

--- app.rb
+++ app.rb
@@ -1,17 +1,76 @@
+require 'rack'
+
 class App
 
+  ALLOWED_PATHS = ['/time'].freeze
+  FORMAT_METHODS_MAPPER = {
+    year: :year,
+    month: :month,
+    day: :day,
+    hour: :hour,
+    minute: :min,
+    second: :sec,
+  }
+
   def call(env)
-    payload
-    [
-        200,
-        { 'Content-Type' => 'text/plain' },
-        ["Welcome aboard! Fucker!\n"]
-    ]
+    @env = env
+    return respond_not_found unless allowed_path?
+    handle_request
   end
 
   private
 
-  def payload
-    sleep 1.3
+  def allowed_path?
+    ALLOWED_PATHS.include?(@env['REQUEST_PATH'])
+  end
+
+  def respond_not_found
+    [404, {}, []]
+  end
+
+  def handle_request
+    query_params = Rack::Utils.parse_nested_query(@env['QUERY_STRING'])
+    return respond_not_found if query_params['format'].nil?
+
+    time_formats = query_params['format'].split(',')
+    time_parts, unknown_formats = prepare_time_parts(time_formats)
+    respond(time_parts, unknown_formats)
+  end
+
+  def prepare_time_parts(time_formats)
+    time = Time.now
+    time_parts = []
+    unknown_formats = []
+
+    time_formats.each do |format|
+      method = FORMAT_METHODS_MAPPER[format.to_sym]
+      if method.nil?
+        unknown_formats << format
+      else
+        time_parts << (time.send method)
+      end
+    end
+
+    [time_parts, unknown_formats]
+  end
+
+  def respond(time_parts, unknown_formats)
+    unknown_formats.empty? ? respond_success(time_parts) : respond_error(unknown_formats)
+  end
+
+  def respond_success(time_parts)
+    [
+        200,
+        { 'Content-Type' => 'text/plain' },
+        [time_parts.join('-') << "\n"]
+    ]
+  end
+
+  def respond_error(unknown_formats)
+    [
+        400,
+        { 'Content-Type' => 'text/plain' },
+        ["Unknown time format [#{unknown_formats.join(', ')}]\n"]
+    ]
   end
 end
--- config.ru
+++ config.ru
@@ -1,8 +1,3 @@
-require_relative 'middleware/app_logger'
-require_relative 'middleware/runtime'
 require_relative 'app'
-
-use AppLogger
-use Runtime
 
 run App.new
